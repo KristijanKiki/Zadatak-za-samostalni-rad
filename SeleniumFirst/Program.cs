4f5214907c19e94c267a3dd11486728e3e19d5a5
KristijanKiki <59533374+KristijanKiki@users.noreply.github.com>
2023-02-03 02:21:10 +0100

    Updated program.cs
    Nadodao sam implementaciju requirementa do 1.4. iz word dokumentacije.

--- SeleniumFirst/Program.cs
+++ SeleniumFirst/Program.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -56,13 +56,50 @@ namespace SeleniumFirst
             Thread.Sleep(2000);
 
             //test 1.3.1.
-            IWebElement ele5 = driver.FindElement(By.ClassName("error-message-container error"));
+            IWebElement ele5 = driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div[1]/div/form/div[3]"));
             String textele5 = ele5.Text;
-            Console.WriteLine(textele5);
+            if (status == true && textUser == "" && textele5 == "Epic sadface: Username is required")
+                Console.WriteLine("Test 1.3.1. :Ako se klikne login button a username textbox je prazan ispise se Epic sadface: Username is required");
+
+            //test 1.3.2.
+            //prvo unesemo bilo sto u username textbox
+            ele2.SendKeys("bilo_koji_user");
+            //kliknemo ponovo login
+            ele4.Click();
+            Thread.Sleep(2000);
+            IWebElement ele6 = driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div[1]/div/form/div[3]"));
+            String textele6 = ele6.Text;
+            if (status == true && textUser == "" && textele6 == "Epic sadface: Password is required")
+                Console.WriteLine("Test 1.3.2. :Ako se klikne login button a username textbox je prazan ispise se Epic sadface: Password is required");
+
+            //test 1.3.3.
+            ele3.SendKeys("random_password");
+            //kliknemo ponovo login
+            ele4.Click();
+            Thread.Sleep(2000);
+            IWebElement ele7 = driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div[1]/div/form/div[3]"));
+            String textele7 = ele7.Text;
+            if (ele3.Text != "secret_sauce" || ele2.Text != "standard_user" || ele2.Text != "performance_glitch_user" || ele2.Text != "problem_user" || ele2.Text != "locked_out_user") ;
+            Console.WriteLine("Test 1.3.3. :Kada se stisne login a ili username ili password nije sa liste dobije se poruka Epic sadface: Username and password do not match any user in this service");
             Thread.Sleep(2000);
 
-            //close the browser  
-            driver.Close();
+            //test 1.4.
+            driver.Navigate().Refresh();
+
+            IWebElement password = driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div[1]/div/form/div[2]/input"));
+            password.SendKeys("secret_sauce");
+            Thread.Sleep(3000);
+            IWebElement username = driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div[1]/div/form/div[1]/input"));
+            username.SendKeys("standard_user");
+            Thread.Sleep(3000);
+            IWebElement loginbutton = driver.FindElement(By.Name("login-button"));
+            loginbutton.Click();
+            Thread.Sleep(3000);
+            IWebElement ele8 = driver.FindElement(By.XPath("/html/body/div/div/div/div[1]/div[2]"));
+            String textele8 = ele8.Text;
+            if (textele8.Contains("PRODUCTS"))
+                Console.WriteLine("Test 1.4. :Kada se unese standardni user i ispravna lozinka otvori se products page");
+
             Console.Write("test case ended ");
         }
     }
